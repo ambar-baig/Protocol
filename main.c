d9277e7feeb5a8a5dfffe3a6ebfc59e6cd2828cd
Babor Baig <90591576+babor-baig@users.noreply.github.com>
2021-09-14 15:58:16 +0500

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,6 @@
 
 
 int main(void){
-    const char* message = "I AM PAKISTANI";
-    const char* algorithm = CONST_MD5;
-    unsigned char digest[64];
-    fprintf(stdout,"Algorithm: %s\n", algorithm);
-    Message_Digest(message, strlen(message), digest, algorithm);
-    fprintf(stdout,"Message: %s\n", message);
-    fprintf(stdout,"Digest: %ld\n", strlen(digest));
-
+    main_command_list();
     return 0;
 }
